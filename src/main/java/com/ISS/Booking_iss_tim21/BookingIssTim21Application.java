8aed5819bd97aacdccc28425632278b441adfe93
Nesa02 <nenad.beric00@gmail.com>
2023-11-20 13:04:47 +0100

    Spring application initialized

---
+++ src/main/java/com/ISS/Booking_iss_tim21/BookingIssTim21Application.java
@@ -0,0 +1,13 @@
+package com.ISS.Booking_iss_tim21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingIssTim21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingIssTim21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ISS/Booking_iss_tim21/BookingIssTim21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISS.Booking_iss_tim21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingIssTim21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
