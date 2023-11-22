ffde43fe12e2f8e16eac0366e1a3e54806f66dec
Borislav Čelar <bora.celar@gmail.com>
2023-11-22 21:17:48 +0100

    work on modeling

--- src/main/java/com/ISS/Booking_iss_tim21/model/Notification.java
+++ src/main/java/com/ISS/Booking_iss_tim21/model/Notification.java
@@ -1,11 +1,40 @@
 package com.ISS.Booking_iss_tim21.model;
 
+<<<<<<< Updated upstream
 import com.ISS.Booking_iss_tim21.model.enumeration.NotificationType;
 
+=======
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+enum NotificationType{
+    RESERVATION_REQUEST,
+    RESERVATION_CANCELLATION,
+    OWNER_REVIEW,
+    ACCOMMODATION_REVIEW,
+    RESERVATION_REQUEST_RESPONSE
+
+}
+@Getter
+@Setter
+@Entity
+>>>>>>> Stashed changes
 public class Notification {
 
-    //User recipient;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User recipient;
+
+    @Enumerated(EnumType.STRING)
     NotificationType type;
+
+    @Column(name = "message", nullable = true)
     String message;
 
 }
--- src/main/java/com/ISS/Booking_iss_tim21/model/Review.java
+++ src/main/java/com/ISS/Booking_iss_tim21/model/Review.java
@@ -1,13 +1,42 @@
 package com.ISS.Booking_iss_tim21.model;
 
+<<<<<<< Updated upstream
 import com.ISS.Booking_iss_tim21.model.enumeration.ReviewType;
 
+=======
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+enum ReviewType{
+    Accommodation,
+    Owner
+}
+
+@Getter
+@Setter
+@Entity
+>>>>>>> Stashed changes
 public class Review {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
     ReviewType type;
 
-    //User reviewer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User reviewer;
+
+    @Column(name = "comment", nullable = true)
     String comment;
+
+    @Column(name = "rating", nullable = true)
     int rating;
 
 }
+
