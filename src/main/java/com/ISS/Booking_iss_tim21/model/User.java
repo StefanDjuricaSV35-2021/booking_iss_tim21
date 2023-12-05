83b9d6358a0f38fe37cd272026cb6e463c6fa32c
Borislav Čelar <bora.celar@gmail.com>
2023-12-05 01:02:47 +0100

    added table tag for user model

--- src/main/java/com/ISS/Booking_iss_tim21/model/User.java
+++ src/main/java/com/ISS/Booking_iss_tim21/model/User.java
@@ -18,6 +18,7 @@ import lombok.experimental.FieldNameConstants;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="`user`")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/ISS/Booking_iss_tim21/repository/FavoriteAccommodationRepository.java
+++ src/main/java/com/ISS/Booking_iss_tim21/repository/FavoriteAccommodationRepository.java
@@ -2,6 +2,8 @@ package com.ISS.Booking_iss_tim21.repository;
 
 import com.ISS.Booking_iss_tim21.model.FavoriteAccommodation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FavoriteAccommodationRepository extends JpaRepository<FavoriteAccommodation,Long> {
 }
