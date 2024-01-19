1242e38acb9ab13464ebed7c6aea45f984e5ce01
Nesa02 <nenad.beric00@gmail.com>
2024-01-19 19:22:42 +0100

    application_test properties set up

--- src/test/java/com/ISS/Booking_iss_tim21/controller/ReservationRequestControllerTest.java
+++ src/test/java/com/ISS/Booking_iss_tim21/controller/ReservationRequestControllerTest.java
@@ -26,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -37,6 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = {ReservationRequestController.class})
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc()
 class ReservationRequestControllerTest {
 
