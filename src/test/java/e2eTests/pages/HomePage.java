0426709a1ae6ab2984bac05f4b9e6fe253d8c2d7
Nesa02 <nenad.beric00@gmail.com>
2024-01-20 12:55:38 +0100

    added login to test

--- src/test/java/e2eTests/pages/HomePage.java
+++ src/test/java/e2eTests/pages/HomePage.java
@@ -1,11 +1,37 @@
 package e2eTests.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
 
     private WebDriver driver;
+    @FindBy(css = ".booking")
+    private WebElement bookingTitle;
+
+    @FindBy(css = "#login")
+    private WebElement loginNavBar;
+
+    @FindBy(css = "#rememberMe")
+    private WebElement rememberMe;
+
+    @FindBy(css = "#loginButton")
+    private WebElement submitLoginButton;
+
+    @FindBy(css = "#emailInput")
+    private WebElement emailInput;
+
+    @FindBy(css = "#passwordInput")
+    private WebElement passwordInput;
+
+    @FindBy(css = "#profileButton")
+    private WebElement profileButton;
 
     private static String PAGE_URL = "http://localhost:4200/homePage";
 
@@ -13,5 +39,23 @@ public class HomePage {
         this.driver = driver;
         driver.get(PAGE_URL);
         PageFactory.initElements(driver, this);
+    }
+
+    public boolean isPageOpened() {
+        return (new WebDriverWait(driver, Duration.ofSeconds(10)))
+                .until(ExpectedConditions.textToBePresentInElement(bookingTitle, "Booʞing"));
+    }
+    public void logIn(String email, String password) {
+        loginNavBar.click();
+        (new WebDriverWait(driver, Duration.ofSeconds(10)))
+                .until(ExpectedConditions.textToBePresentInElement(rememberMe, "Remember me"));
+
+        emailInput.click();
+        emailInput.sendKeys(email);
+
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+
+        submitLoginButton.click();
     }
 }
--- src/test/java/e2eTests/tests/TestCancelReservation.java
+++ src/test/java/e2eTests/tests/TestCancelReservation.java
@@ -2,10 +2,12 @@ package e2eTests.tests;
 
 import e2eTests.pages.HomePage;
 import org.testng.annotations.Test;
-
+import static org.testng.Assert.assertTrue;
 public class TestCancelReservation extends TestBase {
     @Test
     public void test() {
         HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.isPageOpened());
+        homePage.logIn("john.doe@example.com", "admin");
     }
 }
