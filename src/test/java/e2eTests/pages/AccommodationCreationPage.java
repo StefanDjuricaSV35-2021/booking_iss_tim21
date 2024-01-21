3e28a789e835fba2c52307ec5888ff40a530fcd4
StefanDjuricaSV35-2021 <stefandjurica420@gmail.com>
2024-01-21 01:16:35 +0100

    Added tests for e2e

--- src/test/java/e2eTests/pages/AccommodationCreationPage.java
+++ src/test/java/e2eTests/pages/AccommodationCreationPage.java
@@ -1,9 +1,6 @@
 package e2eTests.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -103,18 +100,69 @@ public class AccommodationCreationPage {
 
         pricingForm.submit();
 
-        By tbodySelector = By.cssSelector(".table.table-bordered.mt-3 tbody");
-
+        By tbodySelector = By.cssSelector(".table.table-bordered.mt-3 > tbody");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(tbodySelector));
 
-        List<WebElement> elements = tableElement.findElements(tbodySelector);
-        currentElementNum = elements.size();
-        System.out.println(previousElementNum);
-        System.out.println(currentElementNum);
+        WebElement tbodyElement = driver.findElement(tbodySelector);
+
+        By trSelector = By.cssSelector(".table.table-bordered.mt-3 > tbody > tr");
+
+        try {
+            wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            wait.until(ExpectedConditions.presenceOfElementLocated(trSelector));
+
+            List<WebElement> elements = tbodyElement.findElements(trSelector);
+            currentElementNum = elements.size();
+        } catch (TimeoutException e) {
+            currentElementNum = 0;
+        }
+    }
+
+    public void removePricing(){
+        previousElementNum = currentElementNum;
+
+        pricingForm.submit();
+
+        By tbodySelector = By.cssSelector(".table.table-bordered.mt-3 > tbody");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(tbodySelector));
+
+        WebElement tbodyElement = driver.findElement(tbodySelector);
+
+        By buttonSelector = By.cssSelector(".table.table-bordered.mt-3 > tbody > tr:first-child button");
+
+        try {
+            wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            wait.until(ExpectedConditions.presenceOfElementLocated(buttonSelector));
+
+            WebElement buttonElement = tbodyElement.findElement(buttonSelector);
+            System.out.println(buttonElement.getText());
+            wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            wait.until(ExpectedConditions.elementToBeClickable(buttonElement));
+            buttonElement.click();
+
+            By trSelector = By.cssSelector(".table.table-bordered.mt-3 > tbody > tr");
+
+            try {
+                wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+                wait.until(ExpectedConditions.presenceOfElementLocated(trSelector));
+
+                List<WebElement> elements = tbodyElement.findElements(trSelector);
+                currentElementNum = elements.size();
+            } catch (TimeoutException e) {
+                currentElementNum = 0;
+            }
+        } catch (TimeoutException e) {
+            currentElementNum = previousElementNum;
+        }
     }
 
     public boolean elementAdded(){
         return previousElementNum < currentElementNum;
+    }
+
+    public boolean elementRemoved(){
+        return previousElementNum > currentElementNum;
     }
 }
--- src/test/java/e2eTests/tests/TestCreateAccommodationPricing.java
+++ src/test/java/e2eTests/tests/TestCreateAccommodationPricing.java
@@ -9,6 +9,20 @@ import org.junit.jupiter.api.Assertions;
 import java.time.Duration;
 
 public class TestCreateAccommodationPricing extends TestBase{
+
+    private String email = "stefandjurica420@gmail.com";
+    private String password = "1234";
+
+    private String good_start_date = "01-02-2024";
+    private String good_end_date = "01-05-2024";
+    private String good_price = "123.3";
+
+    private String bad_price = "fjsfjsjfksjfk";
+
+
+    private String second_good_start_date = "01-06-2024";
+    private String second_good_end_date = "01-09-2024";
+
     @Test
     public void test() {
         HomePage homePage = new HomePage(driver);
@@ -18,8 +32,8 @@ public class TestCreateAccommodationPricing extends TestBase{
         LoginPage loginPage = new LoginPage(driver);
         Assertions.assertTrue(loginPage.isLoaded());
 
-        loginPage.inputEmail("stefandjurica420@gmail.com");
-        loginPage.inputPassword("1234");
+        loginPage.inputEmail(email);
+        loginPage.inputPassword(password);
         loginPage.login();
 
         Assertions.assertTrue(homePage.isLoaded());
@@ -33,10 +47,41 @@ public class TestCreateAccommodationPricing extends TestBase{
         accommodationCreationPage.selectPerNight();
         accommodationCreationPage.selectPerGuest();
 
-
-        accommodationCreationPage.inputStartDate("01-02-2024");
-        accommodationCreationPage.inputEndDate("01-03-2024");
-        accommodationCreationPage.inputPricing("123.3");
         accommodationCreationPage.addPricing();
+        Assertions.assertFalse(accommodationCreationPage.elementAdded());
+
+        accommodationCreationPage.inputStartDate(good_start_date);
+        accommodationCreationPage.inputEndDate(good_end_date);
+        accommodationCreationPage.inputPricing(bad_price);
+        accommodationCreationPage.addPricing();
+        Assertions.assertFalse(accommodationCreationPage.elementAdded());
+
+        accommodationCreationPage.inputStartDate(good_start_date);
+        accommodationCreationPage.inputEndDate(good_end_date);
+        accommodationCreationPage.inputPricing(good_price);
+        accommodationCreationPage.addPricing();
+        Assertions.assertTrue(accommodationCreationPage.elementAdded());
+
+        accommodationCreationPage.inputStartDate(good_start_date);
+        accommodationCreationPage.inputEndDate(good_end_date);
+        accommodationCreationPage.inputPricing(good_price);
+        accommodationCreationPage.addPricing();
+        Assertions.assertFalse(accommodationCreationPage.elementAdded());
+
+        accommodationCreationPage.inputStartDate(second_good_end_date);
+        accommodationCreationPage.inputEndDate(second_good_start_date);
+        accommodationCreationPage.inputPricing(good_price);
+        accommodationCreationPage.addPricing();
+        Assertions.assertFalse(accommodationCreationPage.elementAdded());
+
+        accommodationCreationPage.inputStartDate(second_good_start_date);
+        accommodationCreationPage.inputEndDate(second_good_end_date);
+        accommodationCreationPage.inputPricing(good_price);
+        accommodationCreationPage.addPricing();
+        Assertions.assertTrue(accommodationCreationPage.elementAdded());
+
+//        accommodationCreationPage.removePricing();
+//        Assertions.assertTrue(accommodationCreationPage.elementRemoved());
+
     }
 }
